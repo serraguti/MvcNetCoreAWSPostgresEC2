a24142bf1dfa079f7d480df76130605c2ec4e592
serraguti <pacoserranox@gmail.com>
2025-05-08 10:52:41 +0200

    Add project files.

---
+++ MvcNetCoreAWSPostgresEC2/Controllers/DepartamentosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreAWSPostgresEC2.Models;
+using MvcNetCoreAWSPostgresEC2.Repositories;
+
+namespace MvcNetCoreAWSPostgresEC2.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryHospitales repo;
+
+        public DepartamentosController(RepositoryHospitales repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Departamento> departamentos =
+                await this.repo.GetDepartamentosAsync();
+            return View(departamentos);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Departamento departamento =
+                await this.repo.FindDepartamentoAsync(id);
+            return View(departamento);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Departamento dept)
+        {
+            await this.repo.InsertDepartamentoAsync(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcNetCoreAWSPostgresEC2/Repositories/RepositoryHospitales.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCoreAWSPostgresEC2.Data;
+using MvcNetCoreAWSPostgresEC2.Models;
+
+namespace MvcNetCoreAWSPostgresEC2.Repositories
+{
+    public class RepositoryHospitales
+    {
+        private HospitalContext context;
+
+        public RepositoryHospitales(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<Departamento> FindDepartamentoAsync(int idDepartamento)
+        {
+            return await this.context.Departamentos
+                .FirstOrDefaultAsync(x => x.IdDepartamento == idDepartamento);
+        }
+
+        public async Task InsertDepartamentoAsync(int id, string nombre, string localidad)
+        {
+            Departamento departamento = new Departamento();
+            departamento.IdDepartamento = id;
+            departamento.Nombre = nombre;
+            departamento.Localidad = localidad;
+            await this.context.Departamentos.AddAsync(departamento);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
